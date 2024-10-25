3304769c950d1a736001b3c671cc677536cacca5
Chathan Driehuys <chathan@driehuys.com>
2024-10-25 01:50:16 +0000

    Shared NFS storage for media
    Create a pattern for sharing media from an NFS mount to all
    applications.

---
+++ platform/storage.tf
@@ -0,0 +1,37 @@
+resource "kubernetes_persistent_volume" "nfs_media" {
+  metadata {
+    name = "nfs-media"
+  }
+
+  spec {
+    access_modes       = ["ReadWriteMany"]
+    storage_class_name = "nfs-shared"
+    capacity = {
+      storage = "1Gi"
+    }
+    persistent_volume_source {
+      nfs {
+        path   = "/mnt/Primary/Media"
+        server = "freenas.lan.qidux.com"
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "nfs_media" {
+  metadata {
+    name = "nfs-media"
+  }
+
+  spec {
+    access_modes       = ["ReadWriteMany"]
+    storage_class_name = "nfs-shared"
+    volume_name        = "nfs-media"
+
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
